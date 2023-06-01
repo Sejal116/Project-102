4264d8fafa47f5cca3773c8018ba501b169c7a17
Sejal116 <73639182+Sejal116@users.noreply.github.com>
2023-06-01 16:47:57 +0530

    Update and rename organise.py to move_file.py

--- move_file.py
+++ move_file.py
@@ -1,23 +1,23 @@
 import os
 import shutil
 
-from_dir = "/Users/sejal.g/Downloads"
-to_dir = "/Users/sejal.g/Downloads/Document_Files"
+from_dir = "//Users//sejal.g//Downloads"
+to_dir = "//Users//sejal.g//Downloads//Document_Files"
 
 list_of_files = os.listdir(from_dir)
 print(list_of_files)
 
 for file_name in list_of_files:
     name,extension = os.path.splitext(file_name)
-    print(name)
-    print(extension)
+    #print(name)
+    #print(extension)
 
     if extension == '':
         continue 
-    if extension in ['.gif','.png','.jpg','.jpeg','.jfif']:
+    if extension in ['.txt', '.pdf', '.doc', '.docx']:
         path1 = from_dir + '/' + file_name
-        path2 = to_dir + '/' + "Image_Files"
-        path3 = to_dir + '/' + "Image_Files" + '/' + file_name
+        path2 = to_dir + '/' + "Document_Files"
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name
 
         if os.path.exists(path2):
             print("Moving " + file_name + "......") 
